0874a1d2982e352e8c78f56c15c445e3adc95eae
Rob <13954303+robertwayne@users.noreply.github.com>
2020-04-17 08:23:10 -0400

    Version bump for PyPi.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='pyxi',
-        version='0.1.1',
+        version='0.1.2',
         author='Rob Wagner',
         author_email='rob.wagner@outlook.com',
         description='A small library for interacting with `.pyxel` files from PyxelEdit.',
